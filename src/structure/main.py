39ac61c29418faeccf207b678cc4f4caca3b4a13
Heberth <heberthardila@gmail.com>
2017-05-05 18:07:24 -0500

    Cambios minimos..

--- src/structure/main.py
+++ src/structure/main.py
@@ -8,4 +8,4 @@ SONAR_DERECHO_ECHO = 18
 print("Hi i am Pibot")
 sonar_derecho =  Sonar(SONAR_DERECHO_TRIG,SONAR_DERECHO_ECHO)
 
-print "la distancia media es ", sonarderecho.getDistanciaMedia(5)
+print "la distancia media es ", sonar_derecho.getDistanciaMedia(5)
