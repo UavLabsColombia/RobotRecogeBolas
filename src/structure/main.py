c2bcd03e2198d2c0fad1437e9d1bec9af574770b
adress <jaime-aranda@outlook.com>
2017-05-05 19:15:32 -0500

    Movimiento
    creacion del archivo moviento que contiene la clase PuenteH y Motor

--- src/structure/main.py
+++ src/structure/main.py
@@ -1,6 +1,15 @@
-from Sonar import Sonar
+from Movimiento import PuenteH, Motor
+import time
 
+puenteh_derecho = PuenteH(16, 8, 10)
+motor_derecho = puenteh_derecho.motor_a
+motor_derecho.adelante()
+time.sleep(3)
+motor_derecho.atras()
+time.sleep(3)
+motor_derecho.detener()
 
+"""from Sonar import Sonar
 
 SONAR_DERECHO_TRIG = 16
 SONAR_DERECHO_ECHO = 18
@@ -8,4 +17,4 @@ SONAR_DERECHO_ECHO = 18
 print("Hi i am Pibot")
 sonar_derecho =  Sonar(SONAR_DERECHO_TRIG,SONAR_DERECHO_ECHO)
 
-print "la distancia media es ", sonarderecho.getDistanciaMedia(5)
+print "la distancia media es ", sonarderecho.getDistanciaMedia(5)"""
