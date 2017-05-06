2b61ab4001e346cb1cf1a027d32f4c125c458577
Heberth <heberthardila@gmail.com>
2017-05-05 19:57:17 -0500

    Modificaciones minimas

--- src/structure/main.py
+++ src/structure/main.py
@@ -11,8 +11,7 @@ time.sleep(1)
 motor_derecho.detener()
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 SONAR_DERECHO_TRIG = 16
 SONAR_DERECHO_ECHO = 18
 
@@ -20,9 +19,7 @@ print("Hi i am Pibot")
 sonar_derecho =  Sonar(SONAR_DERECHO_TRIG,SONAR_DERECHO_ECHO)
 
 print "la distancia media es ", sonar_derecho.getDistanciaMedia(5)
-=======
-=======
->>>>>>> master
+
 #from Sonar import Sonar
 #
 #SONAR_DERECHO_TRIG = 16
@@ -32,7 +29,3 @@ print "la distancia media es ", sonar_derecho.getDistanciaMedia(5)
 #sonar_derecho =  Sonar(SONAR_DERECHO_TRIG,SONAR_DERECHO_ECHO)
 #
 #print "la distancia media es ", sonarderecho.getDistanciaMedia(5)
-<<<<<<< HEAD
->>>>>>> master
-=======
->>>>>>> master
