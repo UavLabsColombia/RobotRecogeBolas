6cd773de169b01a4fab8e766f1f0c2ebcd8dbbb5
adress <jaime-aranda@outlook.com>
2017-05-05 19:47:09 -0500

    movimiendo de los motores
    ya quedo lista la clase de movimiento de motores

--- src/structure/Movimiento.py
+++ src/structure/Movimiento.py
@@ -4,7 +4,8 @@
 
 import RPi.GPIO as GPIO  # Importamos la libreria RPi.GPIO
 
-GPIO.setmode(GPIO.BOARD)  # Le indicamos a python que la definicion de los pines GPIO se cataloga por numero de pines del 1 al 40
+GPIO.setmode(
+    GPIO.BOARD)  # Le indicamos a python que la definicion de los pines GPIO se cataloga por numero de pines del 1 al 40
 GPIO.setwarnings(False)
 
 
@@ -15,26 +16,26 @@ class PuenteH():
         self.int_2 = int_2
         self.motor_a = Motor(enable_a, int_1, int_2)
 
-    def __init__(self, enable_a, int_1, int_2, int_3, int_4, enable_b):
-        self.enable_a = enable_a
-        self.int_1 = int_1
-        self.int_2 = int_2
-        self.int_3 = int_3
-        self.int_4 = int_4
-        self.enable_b = enable_b
-        self.motor_a = Motor(enable_a, int_1, int_2)
-        self.motor_b = Motor(enable_b, int_1, int_2)
+#    def __init__(self, enable_a, int_1, int_2, int_3, int_4, enable_b):
+#        self.enable_a = enable_a
+#        self.int_1 = int_1
+#        self.int_2 = int_2
+#        self.int_3 = int_3
+#        self.int_4 = int_4
+#        self.enable_b = enable_b
+#        self.motor_a = Motor(enable_a, int_1, int_2)
+#        self.motor_b = Motor(enable_b, int_1, int_2)
 
 
 class Motor():
     def __init__(self, enable, int1, int2):
-        self.pulso = GPIO.PWM(enable, 490)  # Ponemos el pin 16 en modo PWM y enviamos 490 pulsos por segundo
         self.enable = enable
         self.int1 = int1
         self.int2 = int2
         GPIO.setup(int1, GPIO.OUT)  # Pin de salida para In1
         GPIO.setup(int2, GPIO.OUT)  # Pin de salida para In2
         GPIO.setup(enable, GPIO.OUT)  # Pin de salida para EnA
+        self.pulso = GPIO.PWM(enable, 490)  # Ponemos el pin 16 en modo PWM y enviamos 490 pulsos por segundo
 
     def adelante(self, vel):
         self.pulso.start(vel)
--- src/structure/main.py
+++ src/structure/main.py
@@ -3,18 +3,20 @@ import time
 
 puenteh_derecho = PuenteH(16, 8, 10)
 motor_derecho = puenteh_derecho.motor_a
-motor_derecho.adelante()
-time.sleep(3)
-motor_derecho.atras()
-time.sleep(3)
+motor_derecho.adelante(50)
+time.sleep(1)
+motor_derecho.detener()
+motor_derecho.atras(50)
+time.sleep(1)
 motor_derecho.detener()
 
-"""from Sonar import Sonar
 
-SONAR_DERECHO_TRIG = 16
-SONAR_DERECHO_ECHO = 18
-
-print("Hi i am Pibot")
-sonar_derecho =  Sonar(SONAR_DERECHO_TRIG,SONAR_DERECHO_ECHO)
-
-print "la distancia media es ", sonarderecho.getDistanciaMedia(5)"""
+#from Sonar import Sonar
+#
+#SONAR_DERECHO_TRIG = 16
+#SONAR_DERECHO_ECHO = 18
+#
+#print("Hi i am Pibot")
+#sonar_derecho =  Sonar(SONAR_DERECHO_TRIG,SONAR_DERECHO_ECHO)
+#
+#print "la distancia media es ", sonarderecho.getDistanciaMedia(5)
